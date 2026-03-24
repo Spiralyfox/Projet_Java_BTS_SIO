dc26cc5793cdd7cb84441de31adae494b8669533
Spiralyfox <dauriacmatteo@gmail.com@gmail.com>
2026-03-24 15:02:57 +0100

    Exercice_3_completed

--- src/Main.java
+++ src/Main.java
@@ -29,7 +29,7 @@ void main() {
     int AnneeDeNaissance = 2002;
     int age = 2026 - AnneeDeNaissance;
 
-    System.out.println(NomComplet + " est né en " + AnneeDeNaissance + " il a donc " + age + " ans"); */
+    System.out.println(NomComplet + " est né en " + AnneeDeNaissance + " il a donc " + age + " ans");
 
     // Exercice 3
 
@@ -40,6 +40,17 @@ void main() {
         System.out.println("a = b");
     } else {
         System.out.println("a et b sont différents");
-    }
+    } */
+
+    // Exercice 4
+
+    int a = 3, b = 11, c = 5, d = 8;
+
+    System.out.println("1 : (a == b) -> " + (a == b));
+    System.out.println("2 : (b == a + d) -> " + (b == a + d));
+    System.out.println("3 : (b == a + d) && (a == b) -> " + ((b == a + d) && (a == b)));
+    System.out.println("4 : (b == a) || (a == a) -> " + ((b == a) || (a == a)));
+    System.out.println("5 : (c == 5) && false -> " + ((c == 5) && false));
+    System.out.println("6 : ((4 == 4) && (4 == 5)) || (a == a) -> " + (((4 == 4) && (4 ==5)) || (a == a)));
 
 }
