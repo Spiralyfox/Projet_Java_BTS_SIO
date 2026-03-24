fa836953435f2a5d95cf408cef0aa69d0d4a953c
Spiralyfox <dauriacmatteo@gmail.com@gmail.com>
2026-03-24 14:17:57 +0100

    Exercice_1_completed

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
-package main;
-
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main() {
+
+        char Caractere = 'a';
+        int Entier = 1;
+        long EntierLong = 100000000;
+        float NbVirgule = 1.5F;
+        double GrosNbVirgule = 1.999999999999999;
+        boolean VraiFaux = true;
+        String ChaineChar = "abcdefg";
+
+        System.out.println(Caractere);
+        System.out.println(Entier);
+        System.out.println(EntierLong);
+        System.out.println(NbVirgule);
+        System.out.println(GrosNbVirgule);
+        System.out.println(VraiFaux);
+        System.out.println(ChaineChar);
     }
 }
