a021d95c791a56fa030478d316a70d4df2e362b0
Spiralyfox <dauriacmatteo@gmail.com@gmail.com>
2026-03-24 14:40:58 +0100

    Exercice_2_completed

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,35 @@
-public class Main {
+void main() {
 
-    public static void main() {
+    /*        System.out.println("Hello World");
 
-        /*        System.out.println("Hello World"); */
+    // Exercice 1
 
-        char Caractere = 'a';
-        int Entier = 1;
-        long EntierLong = 100000000;
-        float NbVirgule = 1.5F;
-        double GrosNbVirgule = 1.999999999999999;
-        boolean VraiFaux = true;
-        String ChaineChar = "abcdefg";
+    char Caractere = 'a';
+    int Entier = 1;
+    long EntierLong = 100000000;
+    float NbVirgule = 1.5F;
+    double GrosNbVirgule = 1.999999999999999;
+    boolean VraiFaux = true;
+    String ChaineChar = "abcdefg";
 
-        System.out.println(Caractere);
-        System.out.println(Entier);
-        System.out.println(EntierLong);
-        System.out.println(NbVirgule);
-        System.out.println(GrosNbVirgule);
-        System.out.println(VraiFaux);
-        System.out.println(ChaineChar);
-    }
+    System.out.println(Caractere);
+    System.out.println(Entier);
+    System.out.println(EntierLong);
+    System.out.println(NbVirgule);
+    System.out.println(GrosNbVirgule);
+    System.out.println(VraiFaux);
+    System.out.println(ChaineChar); */
+
+    // Exercice 2
+
+    String Nom = "Dupont";
+    String Prenom = "Pierre";
+    String NomComplet = Prenom + " " + Nom;
+
+    int AnneeDeNaissance = 2002;
+    int age = 2026 - AnneeDeNaissance;
+
+    System.out.println(NomComplet + " est né en " + AnneeDeNaissance + " il a donc " + age + " ans");
+
+
 }
